f116120c533945e9d79b70d2bc3c4e7faba4781e
Patrick I <puut4991@yahoo.com>
2022-12-12 12:02:52 -0600

    Fix typing error
    Functionality restored due to typing error

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(editedArray, arg1, arg2, arg3, arg4)
+{
+    let foundIndex;
+    for(let index = 1; index < 5; ++index)
+    {
+        foundIndex = editedArray.indexOf(arguments[index]);
+        if (foundIndex > -1)
+        {
+            editedArray.splice(foundIndex, 1);
+        }
+    }
 
+    return editedArray;
 };
 
 // Do not edit below this line
