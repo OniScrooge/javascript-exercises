3ae2e847c915e3cad01154da2cfec1b35a80819b
Patrick I <puut4991@yahoo.com>
2022-12-12 11:37:55 -0600

    Fix lesson 03
    Lesson 03 had no code, now completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(stringToBeReversed)
+{
+    let fullyReversedString = "";
+    for (let index = (stringToBeReversed.length - 1); index >= 0; --index)
+    {
+        fullyReversedString += stringToBeReversed[index];
+    }
+    return fullyReversedString;
 };
 
 // Do not edit below this line
