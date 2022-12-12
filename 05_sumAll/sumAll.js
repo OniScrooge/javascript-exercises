feb58b667c71ff061297d046b902362c98da1efc
Patrick I <puut4991@yahoo.com>
2022-12-12 12:30:36 -0600

    Fix lesson 05
    Fixed by adding function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(value1, value2)
+{
+    if (value1 < 0 || value2 < 0 || value1 / 1 !== value1 || value2 / 1 !== value2)
+    {
+        return "ERROR";
+    }
 
+    let sumOfAll = 0;
+    if (value1 < value2)
+    {
+        for (let count = value1; count <= value2; ++count)
+        {
+            sumOfAll += count;
+        }
+    }
+    else
+    {
+        for (let count = value2; count <= value1; ++count)
+        {
+            sumOfAll += count;
+        }
+    }
+
+    return sumOfAll;
 };
 
 // Do not edit below this line
