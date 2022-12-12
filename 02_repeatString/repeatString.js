e14eaeb4415eb169aac906151c9543f5f7fb108e
Patrick I <puut4991@yahoo.com>
2022-12-12 11:13:58 -0600

    Test NPM functionality Fix first test
    NPM functions & first test completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(repeatedString, amountOfRepeats)
+{
+    if (amountOfRepeats < 0)
+    {
+        return "ERROR";
+    }
 
+    let fullyRepeatedString = "";
+    for (let count = 0; count < amountOfRepeats; count++)
+    {
+        fullyRepeatedString += repeatedString;
+    }
+    return fullyRepeatedString;
 };
 
 // Do not edit below this line
