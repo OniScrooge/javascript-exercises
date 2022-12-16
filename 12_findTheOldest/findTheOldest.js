8d1f87f23c1acbc7c62fc60d743fe21e64c3ef8d
Patrick I <puut4991@yahoo.com>
2022-12-16 13:09:41 -0600

    Complete assignment

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let years = [];
+    let currentDate = new Date();
+    for (let index = 0; index < people.length; ++index)
+    {
+        if (!people[index].yearOfDeath)
+        {
+            years.push(currentDate.getFullYear() - people[index].yearOfBirth);
+        }
+        else
+        {
+            years.push(people[index].yearOfDeath - people[index].yearOfBirth);
+        }
+    }
 
+    return people[years.indexOf(Math.max(...years))];
 };
 
 // Do not edit below this line
