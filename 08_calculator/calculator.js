9df5649e4e92f8596fa0ffa1d358ed80f49cfb66
Patrick I <puut4991@yahoo.com>
2022-12-16 10:24:54 -0600

    Complete assignment

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(myArray) {
+	if (!myArray) return myArray;
+
+  let total = 0;
+  for (let index = 0; index < myArray.length; ++index)
+  {
+    total += myArray[index];
+  }
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(myArray) {
+  let total = 1;
+  myArray.forEach(element => {
+    total *= element;
+  });
 
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return (a ** b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a <= 1) return 1;
+
+  let total = a;
+  for (let index = (a - 1); index > 0; --index)
+  {
+    total *= index;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
