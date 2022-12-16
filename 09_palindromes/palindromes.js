6eac4eb0f32ec68547b24931eab3ff3feb547dc9
Patrick I <puut4991@yahoo.com>
2022-12-16 10:53:59 -0600

    Complete assignment

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (myString) {
+    myString = myString.replace(/[^\w\s\']|_/g, "").replace(/\s+/g, "").toUpperCase();
 
+    let testString = '';
+    for (let index = (myString.length - 1); index >= 0; --index)
+    {
+        testString += myString[index];
+    }
+
+    if (testString == myString)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 };
 
 // Do not edit below this line
