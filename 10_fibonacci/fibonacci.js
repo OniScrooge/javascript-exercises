2dd0693d3ffca08fc186b3a8afb43eb9e7239bec
Patrick I <puut4991@yahoo.com>
2022-12-16 11:20:27 -0600

    Complete assignment

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(value) {
+    let checkedValue = Number(value);
 
+    if (checkedValue < 0) return "OOPS";
+
+    let fibonacciArray = [0, 1, 1];
+    for (let index = 3; index <= checkedValue; ++index)
+    {
+        fibonacciArray.push(fibonacciArray[index - 1] + fibonacciArray[index - 2]);
+    }
+    return fibonacciArray[checkedValue];
 };
 
 // Do not edit below this line
